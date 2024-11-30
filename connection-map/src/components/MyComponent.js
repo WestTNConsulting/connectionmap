24610ec5a4e69915be74245fc6797542542def39
eakffel <eliza.keffeler@westtn.consulting>
2024-11-30 00:26:30 -0600

    s

--- connection-map/src/components/MyComponent.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react';
-
-const MyComponent = () => {
-  const [state, setState] = useState('Hello World');
-  return <div>{state}</div>;
-};
-
-export default MyComponent;
